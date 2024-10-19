eb5712f20d9549100ebfe9d0398006225d67ce8a
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-10-19 22:45:08 +0000

    chore: create_release.sh & download.sh updated

--- download.sh
+++ download.sh
@@ -7,10 +7,7 @@ aarch64) SUFFIX="aarch64" ;;
 armv6l) SUFFIX="armv6" ;;
 esac
 
-
-
 # screen_control_linux_aarch64.tar.gz 
-
 if [ -n "$SUFFIX" ]; then
 	SCREEN_CTL="screen_control_linux_${SUFFIX}.tar.gz"
 	curl -L -O "https://github.com/mrjackwills/screen_control_backend/releases/latest/download/${SCREEN_CTL}"
